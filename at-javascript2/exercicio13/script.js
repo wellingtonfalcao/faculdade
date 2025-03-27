2d57767f25a85f61e44ffc05a1e00c9a32dc9ba4
Wellington Falcão <wellingtonfalcao@gmail.com>
2025-03-26 21:21:15 -0300

    Exercicio 13 feito

---
+++ at-javascript2/exercicio13/script.js
@@ -0,0 +1,24 @@
+/*
+
+13 Validação de string
+
+Enunciado: Crie uma página que use uma função para validar se uma string está em branco ou não.
+
+Observações:
+
+Use prompt e alert.
+Teste o seu programa com cenários diferentes.
+
+*/
+
+function isBlank(string){
+  return string.trim() === "";
+}
+
+let resultado = isBlank(prompt("Digite algo para validar"));
+
+if(resultado){
+  alert("Não deixe este campo em branco")
+} else {
+  alert("Campo válido")
+}
