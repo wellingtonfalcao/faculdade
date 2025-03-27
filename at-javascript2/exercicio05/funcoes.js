52e7e7e275c779183564ef9a5aa62c246e63eae9
Wellington Falcão <wellingtonfalcao@gmail.com>
2025-03-26 22:06:28 -0300

    Exercicio 5 feito

---
+++ at-javascript2/exercicio05/funcoes.js
@@ -0,0 +1,9 @@
+function tabuada(){
+    tabuada = [];
+    for(let i=1; i <= 10; i++){
+      for(let j=1; j <= 10; j++){
+        tabuada.push(`${i} X ${j} = ${i * j}`);
+      }
+    }
+    return tabuada;
+  }
---
+++ at-javascript2/exercicio05/script.js
@@ -0,0 +1,32 @@
+let tituloHTML = document.querySelector("#title");
+tituloHTML.textContent = "Questao 5: Tabuada";
+
+/*
+
+5 Tabuada
+
+Enunciado: Crie uma página com um programa para mostrar a tabuada de 1 a 10. Exiba o resultado em uma página HTML.
+
+Observações:
+
+Use “document.write” para mostrar as mensagens no próprio HTML de saída: mostre o array gerado.
+
+*/
+window.addEventListener("load", () => {
+  
+  function tabuada(){
+    tabuada = [];
+    for(let i=1; i <= 10; i++){
+      for(let j=1; j <= 10; j++){
+        tabuada.push(`${i} X ${j} = ${i * j}`);
+      }
+    }
+    return tabuada;
+  }
+
+  tabuada = tabuada();
+  
+  document.write(tabuada);
+  
+});
+
